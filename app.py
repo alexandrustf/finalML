b3a86a2a085becf42bd212ae61d428fc97df32bb
alexandrustf <37770439+alexandrustf@users.noreply.github.com>
2020-04-20 20:53:12 +0300

    initial

---
+++ app.py
@@ -0,0 +1,37 @@
+import pickle
+import numpy as np
+from flask import Flask, request
+import json
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+model = None
+app = Flask(__name__)
+
+
+def load_model():
+    global model
+    # model variable refers to the global variable
+    with open('rs_random_forest_model_1.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+
+@app.route('/')
+def home_endpoint():
+    return 'Hello World!'
+
+
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    # Works only for a single sample
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+        data =pd.DataFrame(data)
+#         data = np.array(data)[np.newaxis, :]
+        prediction = model.predict(data)
+        return str(prediction[0])
+
+
+if __name__ == '__main__':
+    load_model()  # load model at the beginning once only
+    app.run(host='0.0.0.0', port=85)
